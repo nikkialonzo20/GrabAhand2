5f75673753d0985d968de938e0045b7b397ac4f0
lynnard <lynnard@reminisense.com>
2017-02-21 11:01:09 +0800

    Changed current location to SWU

--- app/src/main/java/com/example/nikkialonzo/grabahand/MapActivity.java
+++ app/src/main/java/com/example/nikkialonzo/grabahand/MapActivity.java
@@ -2,12 +2,10 @@ package com.example.nikkialonzo.grabahand;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
