d5f3a5454577fd58612218a7052b6cf374c34ed3
lynnard <lynnard@reminisense.com>
2017-02-21 16:57:58 +0800

    Added Sacred Heart and improved dropdown visibility

--- app/src/main/java/com/example/nikkialonzo/grabahand/HospitalActivity.java
+++ app/src/main/java/com/example/nikkialonzo/grabahand/HospitalActivity.java
@@ -24,7 +24,7 @@ public class HospitalActivity extends AppCompatActivity {
 
         list = (Spinner) findViewById(R.id.dropStations);
         ArrayAdapter<CharSequence> countryAdapter = ArrayAdapter.createFromResource(getApplicationContext(),
-                R.array.hospital_list, R.layout.support_simple_spinner_dropdown_item);
+                R.array.hospital_list, R.layout.my_spinner_dropdown);
         list.setAdapter(countryAdapter);
 
 
