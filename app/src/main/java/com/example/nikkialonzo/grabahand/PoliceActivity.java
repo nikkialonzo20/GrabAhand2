b1a8da0192140b9d7680c27eb96651c942c00471
lynnard <lynnard@reminisense.com>
2017-02-22 22:22:26 +0800

    Refactored whole layout Specific station opens a new activity. Settings implemented as menu

--- app/src/main/java/com/example/nikkialonzo/grabahand/PoliceActivity.java
+++ app/src/main/java/com/example/nikkialonzo/grabahand/PoliceActivity.java
@@ -27,32 +27,6 @@ public class PoliceActivity extends AppCompatActivity {
                 R.array.police_list, R.layout.my_spinner_dropdown);
         list.setAdapter(countryAdapter);
 
-
-        Button emergencyButton = (Button) findViewById(R.id.btnImmediate);
-        emergencyButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-                        PoliceActivity.this);
-
-                alertDialogBuilder.setTitle("Request Sent!");
-                alertDialogBuilder.setMessage("A request has been sent to the nearest police station.")
-                        .setCancelable(false)
-                        .setNeutralButton("Okay",new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
-
-                // create alert dialog
-                AlertDialog alertDialog = alertDialogBuilder.create();
-
-                // show it
-                alertDialog.show();
-            }
-        });
-
         Button specificStation = (Button) findViewById(R.id.btnSpecific);
         specificStation.setOnClickListener(new View.OnClickListener() {
             @Override
