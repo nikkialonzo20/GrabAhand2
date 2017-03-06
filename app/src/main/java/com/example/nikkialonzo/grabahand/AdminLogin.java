3995e3e7f2268a8a043dbfaf04c01fb0ef98bedd
lynnard <lynnard@reminisense.com>
2017-03-06 20:53:42 +0800

    Added login admin page

--- app/src/main/java/com/example/nikkialonzo/grabahand/AdminLogin.java
+++ app/src/main/java/com/example/nikkialonzo/grabahand/AdminLogin.java
@@ -1,20 +1,10 @@
 package com.example.nikkialonzo.grabahand;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 /**
  * Created by gerard on 2/28/2017.
@@ -25,7 +15,6 @@ public class AdminLogin extends AppCompatActivity {
     EditText email;
     EditText password;
     Button login;
-    GrabEndpoint apiService;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,51 +24,6 @@ public class AdminLogin extends AppCompatActivity {
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
         login = (Button) findViewById(R.id.login);
-        apiService = new RestClient().getApiService();
 
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                LoginInfo loginInfo = new LoginInfo(email.getText().toString(),password.getText().toString());
-                Call<LoginResult> call = apiService.loginUser(loginInfo);
-                call.enqueue(new Callback<LoginResult>() {
-                    @Override
-                    public void onResponse(Call<LoginResult> call, Response<LoginResult> response) {
-                        LoginResult loginResult = response.body();
-                        try {
-                            if (loginResult.getSuccess() == 1) {
-                                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(AdminLogin.this);
-                                SharedPreferences.Editor editor = sharedPreferences.edit();
-
-                                editor.putString("ADMIN_NAME", loginResult.getAdmin().getName());
-                                editor.putString("ADMIN_EMAIL", loginResult.getAdmin().getEmail());
-                                editor.putInt("ADMIN_PHONE", loginResult.getAdmin().getPhone());
-                                editor.putString("INSTITUTION_NAME", loginResult.getAdmin().getInstitutionName());
-                                editor.putInt("JOB_ID", loginResult.getAdmin().getJobId());
-                                editor.apply();
-                                Toast.makeText(AdminLogin.this, "Success",Toast.LENGTH_SHORT).show();
-                            }else{
-                                Toast.makeText(AdminLogin.this, loginResult.getMsg() ,Toast.LENGTH_SHORT).show();
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<LoginResult> call, Throwable t) {
-                        Toast.makeText(AdminLogin.this, "Login Failed.",Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-            }
-        });
-    }
-
-    @Override
-    public void onBackPressed() {
-        startActivity(new Intent(AdminLogin.this, MainActivity.class));
-        finish();
     }
 }
--- app/src/main/java/com/example/nikkialonzo/grabahand/MapActivity.java
+++ app/src/main/java/com/example/nikkialonzo/grabahand/MapActivity.java
@@ -1,21 +1,30 @@
 package com.example.nikkialonzo.grabahand;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by gerard on 2/20/2017.
  */
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private SupportMapFragment mapFragment;
 
@@ -29,6 +38,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
 
@@ -38,5 +48,33 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         googleMap.addMarker(new MarkerOptions().position(SWU)
                 .title("Southwestern University"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SWU, 15.00f));
+
+        googleMap.setOnMarkerClickListener(this);
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+
+        String name = "Name: ";
+        String number = "Phone: ";
+        String email = "Email: ";
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+
+        dialog.setTitle("EMERGENCY");
+        dialog.setMessage(name + "\n \n" + number + "\n \n" + email)
+                .setNeutralButton("RESPOND", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getApplicationContext(), "A feedback has been sent to the user that help is on the way.", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+        AlertDialog dialog1 = dialog.create();
+
+
+        dialog1.show();
+        return true;
+
+
     }
 }
