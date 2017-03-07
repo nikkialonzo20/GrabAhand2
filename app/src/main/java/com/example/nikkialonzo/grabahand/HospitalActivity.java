500310fe466cfe1c078f89694bb197d3d49b460c
NikkiAlonzo <nikkialonzo12.na@gmail.com>
2017-03-07 23:20:13 +0800

    master

--- app/src/main/java/com/example/nikkialonzo/grabahand/HospitalActivity.java
+++ app/src/main/java/com/example/nikkialonzo/grabahand/HospitalActivity.java
@@ -3,14 +3,12 @@ package com.example.nikkialonzo.grabahand;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.Spinner;
 
 public class HospitalActivity extends AppCompatActivity {
--- app/src/main/java/com/example/nikkialonzo/grabahand/MainActivity.java
+++ app/src/main/java/com/example/nikkialonzo/grabahand/MainActivity.java
@@ -4,13 +4,12 @@ package com.example.nikkialonzo.grabahand;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
